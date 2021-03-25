01bda96d454fecdf40a9097d16a2cb74477b6cee
Franco Ortega <francopdx@gmail.com>
2021-03-25 14:05:58 -0700

    add logic to ask questions 2 and 3, collect answers, and tally score

--- app.ts
+++ app.ts
@@ -17,7 +17,9 @@ console.log('Button was clicked!')
         const userName: string = prompt('What is your name?');
 
         const questionOne = `Hey ${userName}! Does Lavinia get married?`;
-        
+        const questionTwo = `How about this one, ${userName}. Is the new found city called Lantuma?`;
+        const questionThree = `One more to go, ${userName}! Does Lavinia turn into a cat?`;
+
         const answerOne: string = question(userName, questionOne);
         const scoreOne: boolean = countsAsYes(answerOne);
 
@@ -28,6 +30,32 @@ console.log(scoreOne);
         if(scoreOne) {
             quizScore++;
         };
+
+console.log(quizScore);
+
+        const answerTwo: string = question(userName, questionTwo);
+        const scoreTwo: boolean = countsAsYes(answerTwo);
+
+console.log(answerTwo);
+console.log(scoreTwo);
+        
+
+        if(scoreTwo) {
+            quizScore++;
+        }
+
+console.log(quizScore);
+
+        const answerThree: string = question(userName, questionThree);
+        const scoreThree: boolean = countsAsYes(answerThree);
+
+console.log(answerThree);
+console.log(scoreThree);
+        
+
+        if(!scoreThree) {
+            quizScore++;
+        }
 
 console.log(quizScore);
 
--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,8 @@ startButton.addEventListener('click', function () {
     else {
         var userName = prompt('What is your name?');
         var questionOne = "Hey " + userName + "! Does Lavinia get married?";
+        var questionTwo = "How about this one, " + userName + ". Is the new found city called Lantuma?";
+        var questionThree = "One more to go, " + userName + "! Does Lavinia turn into a cat?";
         var answerOne = question(userName, questionOne);
         var scoreOne = countsAsYes(answerOne);
         console.log(userName);
@@ -20,6 +22,22 @@ startButton.addEventListener('click', function () {
             quizScore++;
         }
         ;
+        console.log(quizScore);
+        var answerTwo = question(userName, questionTwo);
+        var scoreTwo = countsAsYes(answerTwo);
+        console.log(answerTwo);
+        console.log(scoreTwo);
+        if (scoreTwo) {
+            quizScore++;
+        }
+        console.log(quizScore);
+        var answerThree = question(userName, questionThree);
+        var scoreThree = countsAsYes(answerThree);
+        console.log(answerThree);
+        console.log(scoreThree);
+        if (!scoreThree) {
+            quizScore++;
+        }
         console.log(quizScore);
     }
     ;
