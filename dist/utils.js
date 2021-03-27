07757afb3acf0a4a250ab9c6a1b57a6a2341b7f0
Franco Ortega <francopdx@gmail.com>
2021-03-27 16:04:42 -0700

    compile updates

--- dist/utils.js
+++ dist/utils.js
@@ -16,18 +16,21 @@ var question = function (question) {
 var completeQuiz = function (userName) {
     return alert("Congratulations, " + userName + ", you have completed the quiz.");
 };
+var calculatePercentage = function (score) {
+    return Math.floor(score / 3 * 100);
+};
 var quizResults = function (userName, score) {
     if (score === 3) {
-        return "Congratulations, " + userName + "! You correctly answered all " + score + " out of 3 for " + Math.floor(score / 3 * 100) + "%!!";
+        return "Congratulations, " + userName + "! You correctly answered all " + score + " out of 3 for " + calculatePercentage(score) + "%!!";
     }
     else if (score === 2) {
-        return "Good job, " + userName + "! You correctly answered " + score + " out of 3 for " + Math.floor(score / 3 * 100) + "%.";
+        return "Good job, " + userName + "! You correctly answered " + score + " out of 3 for " + calculatePercentage(score) + "%.";
     }
     else if (score === 1) {
-        return "Good try, " + userName + ". You correctly answered " + score + " out of 3 for " + Math.floor(score / 3 * 100) + "%.";
+        return "Good try, " + userName + ". You correctly answered " + score + " out of 3 for " + calculatePercentage(score) + "%.";
     }
     else {
-        return "You correctly answered " + score + " out of 3 for " + Math.floor(score / 3 * 100) + "%. Better luck next time, " + userName + ".";
+        return "You correctly answered " + score + " out of 3 for " + calculatePercentage(score) + "%. Better luck next time, " + userName + ".";
     }
     ;
 };
